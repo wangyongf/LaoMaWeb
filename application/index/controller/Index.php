b783c71bd12fae1aa9e0a6ee885a2e162f40ca47
wangyongf <scottwang1996@gmail.com>
2016-09-30 16:25:19 +0800

    change line-seperator to /n

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,16 +1,14 @@
 <?php
 namespace app\index\controller;
 
-use think\Loader;
-use \traits\controller\Jump;
+use \think\View;
 
 class Index
 {
     public function index()
     {
-        $Test = new \my\Test();
-        $res = $Test->sayHello();
-        return $res;
+        $view = new View();
+        return $view->fetch();
     }
 
 }
